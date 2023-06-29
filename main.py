509b9245b0d399267245c92855275dfb1dd4a9ca
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-06-29 15:03:28 +0100

    Cleanning and refactoring. Initial model works

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
-import sys
-import random
-import numpy as np
-import pandas as pd
-import numpy as np
+import copy
+import ipyvolume as ipv
 import json
 import matplotlib.pylab as plt
-import ipyvolume as ipv
-import copy
+import numpy as np
+import pandas as pd
+import random
+import sys
 from IPython.display import Image
 from scipy import optimize
 
 ##### From tyssue
-from tyssue import Sheet, SheetGeometry, History, EventManager
-from tyssue import SheetGeometry as geom
-from tyssue import PlanarGeometry as geom
-from tyssue.draw.plt_draw import create_gif
-from tyssue.draw.plt_draw import plot_forces
+from tyssue import PlanarGeometry, Sheet, SheetGeometry, History, EventManager
 from tyssue.draw import sheet_view
-from tyssue.dynamics import effectors, model_factory
-from tyssue.dynamics import PlanarModel
-from tyssue.dynamics import SheetModel as model
+from tyssue.draw.plt_draw import create_gif, plot_forces
+from tyssue.dynamics import effectors, model_factory, PlanarModel
 from tyssue.generation import three_faces_sheet
-from tyssue.solvers.viscous import EulerSolver
-from tyssue.solvers.quasistatic import QSSolver
-from tyssue.draw import sheet_view
-from tyssue.draw.plt_draw import plot_forces
 from tyssue.io import hdf5
-from tyssue.draw import sheet_view
-from tyssue.geometry.planar_geometry import PlanarGeometry as geom
 from tyssue.solvers.quasistatic import QSSolver
-from tyssue.dynamics.planar_vertex_model import PlanarModel as model
+from tyssue.solvers.viscous import EulerSolver
 from tyssue.stores import load_datasets
 from tyssue.topology.sheet_topology import remove_face, cell_division
 
@@ -37,32 +25,43 @@ from tyssue.topology.sheet_topology import remove_face, cell_division
 import vertexModel
 
 
-#### Definition of the sheet
-geom_original  = SheetGeometry # This may need to change
+#### Initialize Model
 
-## https://tyssue.readthedocs.io/en/latest/_modules/tyssue/dynamics/effectors.html
-# 
-model_original = model_factory([    
+## Definition of the sheet
+# PlanarGeometry: Geometry methods for 2D planar cell arangements
+# SheetGeometry: Geometry definitions for 2D sheets in 3D
+# BulkGeometry: Geometry functions for 3D cell arangements
+geom_init  = PlanarGeometry
+
+## Defining energy contributions
+# https://tyssue.readthedocs.io/en/latest/_modules/tyssue/dynamics/effectors.html
+model_init = model_factory([    
     effectors.FaceAreaElasticity,
-    effectors.LineTension
-    #effectors.LengthElasticity
-    #effectors.PerimeterElasticity
-    #effectors.FaceVolumeElasticity
-    #effectors.CellAreaElasticity
-    #effectors.CellVolumeElasticity
-    #effectors.LumenVolumeElasticity
-    #effectors.LineTension
-    #effectors.FaceContractility
+    effectors.LineTension,
+    #effectors.LengthElasticity,
+    effectors.PerimeterElasticity,
+    #effectors.CellAreaElasticity,
+    #effectors.FaceContractility,
     effectors.BarrierElasticity
     #effectors.LineViscosity
-    ]) 
-cellmap_original = Sheet.planar_sheet_3d('cellmap_original', 20, 20, 1, 1) # This may need to change
-cellmap_original.sanitize ( trim_borders=True, order_edges=True )
-geom_original.update_all(cellmap_original)
+    ])
 
-history_original = History(cellmap_original, extra_cols={"edge":["dx", "dy"]})
+## Size of the patch
+# noise = 0 -> hexagonal pattern
+# noise = 1 -> random voronoi
+cellmap_init = Sheet.planar_sheet_2d('tissue', 20, 20, 1, 1, noise=0.3)
+cellmap_init.remove(cellmap_init.cut_out([[1, 20], [1, 20]]), trim_borders=True)
+
+## Update geometry with the patch
+geom_init.update_all(cellmap_init)
+cellmap_init.reset_index()
+cellmap_init.reset_topo()
+fig, ax = sheet_view(cellmap_init, mode="quick")
+
+## Init history object
+history_init = History(cellmap_init, extra_cols={"edge":["dx", "dy"]})
 
 ## RUN
-[cellmap_H, geom_H, model_H, history_H] = vertexModel.runModel(cellmap_original, geom_original, model_original, history_original, 
+[cellmap_H, geom_H, model_H, history_H] = vertexModel.runModel(cellmap_init, geom_init, model_init, history_init, 
                                                        face_elasticity = 10, prefered_area = 0.6, lower_line_tension = 1, 
                                                        higher_line_tension = 100, endTime = 30)
